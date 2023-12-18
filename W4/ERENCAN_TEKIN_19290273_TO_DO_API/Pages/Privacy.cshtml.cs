10dbd70c8ca094247fa6baebaca9f6b197b4def4
erencantekin <88515143+erencantekin@users.noreply.github.com>
2023-12-18 20:52:27 +0300

    Fourth Control
    Fourth Control is complete.

---
+++ W4/ERENCAN_TEKIN_19290273_TO_DO_API/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ERENCAN_TEKIN_19290273_TO_DO_API.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
